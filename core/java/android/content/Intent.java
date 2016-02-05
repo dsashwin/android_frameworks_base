b496aa5af30122f569fb93be44394d6a1159b305
Roman Birg <roman@cyngn.com>
2016-02-04 16:28:00 -0800

    SystemUI: handle camera launch gesture from keyhandler
    Change-Id: Iba9211b7d342f618c4082c34d11663d89861607a
    Co-Authored-By: Michael Bestas <mikeioannina@cyanogenmod.org>

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -7159,6 +7159,15 @@ public class Intent implements Parcelable, Cloneable {
     @FlaggedApi(com.android.eyedropper.Flags.FLAG_ENABLE_EYE_DROPPER_API)
     public static final String EXTRA_COLOR = "android.intent.extra.COLOR";
 
+    /**
+     * Broadcast action: notify the system that the user has performed a gesture on the screen
+     * to launch the camera. Broadcast should be protected to receivers holding the
+     * {@link Manifest.permission#STATUS_BAR_SERVICE} permission.
+     * @hide
+     */
+    public static final String ACTION_SCREEN_CAMERA_GESTURE =
+            "android.intent.action.SCREEN_CAMERA_GESTURE";
+
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
     // Intent flags (see mFlags variable).
